5b47d9368ed746b3ed5cc281103e49163119b0e8
Rafael Junior <rafas8921@gmail.com>
2025-11-27 12:15:49 -0500

    Añadido archivo index.html con estructura básica y formulario de reporte. Modificada configuración de seguridad para permitir inicio de sesión con formulario.

--- src/main/java/com/emergency_system/emergency_system/config/SecurityConfig.java
+++ src/main/java/com/emergency_system/emergency_system/config/SecurityConfig.java
@@ -14,9 +14,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
           .csrf(csrf -> csrf.disable())
-          .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-          .httpBasic(httpBasic ->{
-          });
+          .authorizeHttpRequests(auth -> auth
+              .anyRequest().authenticated()
+          )
+          .formLogin(login -> {});
         return http.build();
     }
     
